82a0ccea8ac794f0acc970450e663461c02a2890
Drew Boynton <drewboynton@Drews-MacBook-Air.local>
2025-01-12 19:48:17 -0700

    Update data flow patterns and make ExerciseTemplate conform to Identifiable

--- SimpleFitness/Core/Models/ExerciseTemplate+CoreDataClass.swift
+++ SimpleFitness/Core/Models/ExerciseTemplate+CoreDataClass.swift
@@ -2,6 +2,26 @@ import Foundation
 import CoreData
 
 @objc(ExerciseTemplate)
-public class ExerciseTemplate: NSManagedObject {
-    // Custom methods can be added here
+public class ExerciseTemplate: NSManagedObject, Identifiable {
+    public var id: UUID {
+        get {
+            id_ ?? UUID()
+        }
+        set {
+            id_ = newValue
+        }
+    }
+    
+    // Helper computed properties
+    public var displayName: String {
+        name ?? "Unnamed Exercise"
+    }
+    
+    public var exerciseCount: Int {
+        (exercises?.count ?? 0)
+    }
+    
+    public var lastUsed: Date? {
+        lastUsedDate
+    }
 } 
